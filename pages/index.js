ffc22aa155764328ece343d5b64cbd730a30e8cc
Peter-Paul <ppmunga@gmail.com>
2023-11-01 19:24:42 +0300

    First commit

--- pages/index.js
+++ pages/index.js
@@ -3,17 +3,10 @@ import { Inter } from 'next/font/google'
 import Navbar from '@/components/Navbar'
 import Hero from './Hero'
 import Card from './Card'
-import Hero1 from './Hero1'
-import Hero2 from './Hero2'
-import Hero4 from './Hero4'
-import Hero5 from './Hero5'
-import Steps from './Steps'
 
-import Hero6 from './Hero6'
 import Hero8 from './Hero8'
 import Hero9 from './Hero9'
-import Hero7 from './Hero7'
-import PreLoader from '@/components/PreLoader'
+
 import Footer from '@/components/Footer'
 import Hero10 from './Hero10'
 
